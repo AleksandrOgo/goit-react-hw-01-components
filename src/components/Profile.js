1577cf1e4e54e7d2f1a0785edd35f56355e1c277
AleksandrOgo <ogorodnik.aleksandr@icloud.com>
2023-08-17 21:38:05 +0300

    Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,30 @@
-import React from 'react'
+export const Profile = ({username, tag, location, avatar, stats}) => {
+    return (
+    <div class="profile">
+  <div class="description">
+    <img
+      src={avatar}
+      alt={username}
+      class="avatar"
+    />
+                <p class="name">{username}</p>
+                <p class="tag">@{tag}</p>
+    <p class="location">{location}</p>
+  </div>
 
-export default function Profile() {
-  return (
-    <div>Profile</div>
-  )
-}
+  <ul class="stats">
+    <li>
+      <span class="label">Followers</span>
+                    <span class="quantity">{stats.followers}</span>
+    </li>
+    <li>
+      <span class="label">Views</span>
+      <span class="quantity">{stats.views}</span>
+    </li>
+    <li>
+      <span class="label">Likes</span>
+      <span class="quantity">{stats.likes}</span>
+    </li>
+  </ul>
+</div>
+)}
